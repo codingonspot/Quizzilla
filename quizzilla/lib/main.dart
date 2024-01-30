d6b6a84341c712b3d28a7842b8f1fec19edf16b6
codingonspot <ishanminio1@gmail.com>
2024-01-30 12:32:20 +0530

    On main branch

--- quizzilla/lib/main.dart
+++ quizzilla/lib/main.dart
@@ -95,6 +95,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                //The user picked true.
                 checkAnswer(true);
               },
             ),
